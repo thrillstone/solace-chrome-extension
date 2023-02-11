79a8768cd9f89fb9b3e19fc3917047eff578447f
Kevin Lidstone <kevin.lidstone@solace.com>
2023-02-11 11:42:27 -0500

    Solac-ify it

--- README.md
+++ README.md
@@ -1,70 +1,23 @@
-mqtt2chrome
+Solace Chrome Extension Example for uOttaHack
 ===========
-
---
-Please NOTE this extension isn't maintained anymore, please use at own risk.
---
-
-by @bordignon, and with help from @clawsicus
-
-You can do what you want with the code as long as you provide attribution back to me and don’t hold me liable!
+This repo is forked from https://github.com/matbor/mqtt2chrome
 
 ##Overview
 
-This extension connects to a MQTT broker using websockets and displays any received messages using Chrome's built-in notifications function.
+This extension connects to a Solace Event Broker using the MQTT protocol over websockets and displays any received messages using Chrome's built-in notifications function.
 
 Requires chrome "notifications" permission.
 
-Tested on Windows, Linux and OSX.
-
-Currently doesn't support TLS, will except pull requests for it. It does however support username/password, but until I fix it the password is stored in the extenstions localstorage but in an unencrypted format. (ie. it is stored in clear text! You have been warned.)
-
-*Screenshot of what the notification looks like. Observe the notification box in bottom right and also the connection icon near the address bar. The connection icon will change if the connection is lost.*
 ![notification box](https://raw.github.com/matbor/mqtt2chrome/master/screenshots/message%20recieved.png)
 
 
-##Installation
+##Setup
 
-**Option-A**
-
-We are now in the 'chrome web store', but check the version as it might be behind. Simply visit the chrome webstore and search for mqtt2chrome [or click here](https://chrome.google.com/webstore/detail/mqtt2chrome/jfcmchhmjkddfoekjbkljcfpdjnpailh)
-
-
-**Option-B** *(Below is if you are running from the source from github)*
-
-1. Download all the files to a directory.
+1. Clone this repo
+2. Edit the background.js file and change the hostname and password
 2. In Chrome, goto Tools -> Extenstions.
 3. Tick the 'Developer mode' box, then click 'Load unpacked extension...'
-4. Select the directory where you have downloaded the extension.
-5. The extension will automatically load and connect using the default settings.
-6. The settings page will open allowing you to change them to suit your configuration.
-7. Click Save & Close. Note: if you changed the broker, port or subtopic settings you need to click the MQTT2Chrome toolbar to trigger a reconnect.
-
-*Screenshot shows where the options are to install the extension in developer mode.*
-![example of install](https://raw.github.com/matbor/mqtt2chrome/master/screenshots/howto%20load.png)
-
-
-##Usage
-
-This extension expects to receive a specific JSON formatted message payload:
-
-    { "sub": "", "txt": "", "img": "", "url":""}
-
-Where:
-
-  * sub (required) - represents the notification heading used in the notification.
-  * txt (required) - represents the text used in the notification. If you put a url in here it can be opened by clicking on the notification box.
-  * img (optional) - represents the image filename from within the 'thumbnails/' directory of the extension. For example; alert.png, warning.png, etc. NOTE; it looks like Chrome doesn't let you add your own images anymore into the thumbnails directory if you are using the version from the chrome store (Option-A for install).  If you are running a unpacked version (option-B for install) from github here you wont have that problem.
-  * url (optional) - NEW!! this is a url to a webpage. When you click on the notification it will open the URL in a new tab.
-
-Example:
-
-    {"sub": "thanks for trying mqtt2chrome","txt":"Check out more at http://twitter.com/bordignon","img":"alert.png","url":" http://twitter.com/bordignon"}
-
-![message](https://raw.github.com/matbor/mqtt2chrome/master/screenshots/message.png)
-
-##Additional
-Check out the following projects that this works well with;
-* https://github.com/matbor/twitter2mqtt
-* https://github.com/jpmens/mqttwarn  <<< HIGHLY recommend!
-* https://github.com/openhab/openhab/
+4. Select the directory where you have cloned the extension.
+6. If you entered your credentials correctly, a notification will show up saying you're connected and subscribed to testexample topic
+7. If you publish a message to the broker to the testexample topic, you will get a notification in chrome showing the message
+You can use this link to publish: https://codepen.io/solacecloud/pen/bxdrbL. Fill out the connection details under the "show advanced settings".
--- background.js
+++ background.js
@@ -31,48 +31,32 @@ todo;
 // This variable is used when generating unique notification identifiers.
 var notificationId = 0;
 var urls = {};
+importScripts("mqttws31.js");
 /*
 Conditionally initialize the options to reasonable defaults and
 open the options in a new tab for the user to configure appropriately.
 */
-if (!localStorage.isInitialized)
-{
-  // Initialize extension options to sone default values.
-  localStorage.broker = "test.mosquitto.org";       // broker websocket address
-  localStorage.port = "80";                         // broker websocket port
-  localStorage.username = "";                       // broker username, leave blank for none
-  localStorage.password = "";                       // broker password, leave blank for none  
-  localStorage.subtopic = "/mqtt2chrome/messages";  // subtopic to subscribe to
-  localStorage.reconnectTimeout = "10";             // Reconnect to broker after this many seconds
-  localStorage.clearNotifications = true;           // Enable automatic clearing of notifications
-  localStorage.notificationTimeout = "1800";          // Automatically clear notifications after this many seconds
-  localStorage.isInitialized = true;                // Only initialise once
-  chrome.tabs.create({url: chrome.extension.getURL('options.html')});
-}
-
 
 //connect to the broker function
 function connect()
 {
-  console.log("Connecting to `" + localStorage.broker + "` on port `" + localStorage.port + "`");
-  var clientId = "myclientid_" + parseInt(Math.random() * 100, 10);
-
-  client = new Messaging.Client(localStorage.broker,
-                                parseInt(localStorage.port),
-                                clientId);
+  client = new Paho.Client("<host here (remove the protocol and port)>",
+                                Number(8443),
+                                "solace-chrome-extension-example");
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
 
   var connectOptions = new Object();
-  connectOptions.useSSL = false;
+  connectOptions.useSSL = true;
   connectOptions.cleanSession = true;
   connectOptions.onSuccess = onConnect;
-  if (localStorage.username != "") {
-    connectOptions.userName = localStorage.username;
-  }
-  if (localStorage.password != "") {
-    connectOptions.password = localStorage.password;
-  }
+  connectOptions.keepAliveInterval = 60;
+  connectOptions.timeout = 3;
+  connectOptions.reconnect = true;
+  connectOptions.userName = "solace-cloud-client";
+  connectOptions.password = "<password-here>";
+  connectOptions.timeout = 10000;
+  console.log("Connecting to", client.hostname, client, connectOptions);
   client.connect(connectOptions);
 }
 
@@ -92,7 +76,7 @@ function clearedCallback(wasCleared) {
 }
 
 // create the popupNotification in Chrome
-function popupNotification(poptitle, popmessage, popicon, popurl)
+function popupNotification(poptitle, popmessage, popicon)
 
 {
   options = {
@@ -104,18 +88,8 @@ function popupNotification(poptitle, popmessage, popicon, popurl)
   };
   var n_id = "id" + notificationId++;
   
-  chrome.notifications.create(n_id, options, createdCallback);
-  
-  //add the url to the array.
-  //if(typeof popurl === 'undefined'){ popurl = 'http://google.com' };
-  urls[n_id] = popurl;
+  chrome.notifications.create(options, createdCallback);
 
-  if (JSON.parse(localStorage.clearNotifications))
-  {
-    // discard notification after timeout period
-    window.setTimeout(function() {clearNotification(n_id)},
-                      parseInt(localStorage.notificationTimeout) * 1000);
-  }
 }
 
 /*
@@ -123,10 +97,9 @@ Once connected to the broker, subscribe to the subtopic.
 */
 function onConnect()
 {
-  console.log("Connected to broker, subscribing for subtopic: " + localStorage.subtopic);
-  client.subscribe(localStorage.subtopic);
-  chrome.browserAction.setIcon({path:"icon.png"});
-  popupNotification("MQTT Broker connected","","icon.png");
+  client.subscribe("exampletopic");
+  chrome.action.setIcon({path:"icon.png"});
+  popupNotification("MQTT Broker connected","Connection established to exampletopic","icon.png");
 
   /*
   //uncomment the below if you want to publish to a topic on connect
@@ -145,12 +118,8 @@ function onConnectionLost(responseObject)
   if (responseObject.errorCode !== 0)
   {
     console.log("Connection to broker lost:"+responseObject.errorMessage);
-    chrome.browserAction.setIcon({path:"icon_noconnection.png"});
+    chrome.action.setIcon({path:"icon_noconnection.png"});
     popupNotification("MQTT Broker disconneced","Reason: "+responseObject.errorMessage,"icon_noconnection.png");
-    if (localStorage.reconnectTimeout !== 0)
-    {
-      window.setTimeout(connect, parseInt(localStorage.reconnectTimeout, 10) * 1000); //wait X seconds before trying to connect again.
-    }
   }
 };
 
@@ -160,81 +129,9 @@ as a Chrome notification.
 */
 function onMessageArrived(message) {
   //console.log("New Message has Arrived: "+message.destinationName + " " + message.payloadString);
-  try
-  {
-    var msg = JSON.parse(message.payloadString);
-    var thumbnail;
-    var url;
-    //console.log(msg.sub);
-    //console.log(msg.txt);
-    //console.log(msg.img);
-    //console.log(msg.url);
-    
-    // checking to see if the url json object is there first    
-    if(!(msg.hasOwnProperty('url')) || msg.url == "") 
-    {
-      console.log('we don\'t have a url');
-      url = null;
-    } 
-    // checking to see if the img json object is there
-    if(!(msg.hasOwnProperty('img')) || msg.img == "") 
-    {
-      console.log('we don\'t have a img');
-      thumbnail = "icon.png"; //default icon
-    } 
-    else if (msg.hasOwnProperty('img'))
-    {
-      thumbnail = 'thumbnails/' + msg.img;
-    }
-      // create the pop
-    popupNotification(msg.sub,msg.txt,thumbnail,msg.url);
-
-  }
-  catch (e)
-  {
-    // Invalid JSON format in message, display the message anyway.
-    console.log('Invalid JSON message format, displaying message anyway!');
-    popupNotification(message.destinationName,message.payloadString,"icon.png");
-  }
+  console.log(message);
+    popupNotification("Message Received",message.payloadString,"icon.png");
 };
-
-//used for when you click the MQTT extension icon in the menu bar
-//it will disconnect the broker and then reconnect
-chrome.browserAction.onClicked.addListener(function ()
-{
-  console.log('MQTT extension icon clicked, disconnect and reconnect to broker in progress');
-  try
-  {
-    client.disconnect();
-    chrome.browserAction.setIcon({path:"icon_noconnection.png"});
-
-  }
-  catch (e)
-  {
-    console.log(e);
-  };
-  window.setTimeout(connect, 2000);
-});
-
-//open the url in a new tab
-function notificationClicked(id) {
-  console.log("The notification '" + id + "' was clicked" );
-  if(!(typeof urls[id] === 'undefined'))
-    {
-      chrome.tabs.create({url: urls[id]});
-    };
-}
-
-function notifyClose(id, byUser) {
-  // Clean up the matching
-  console.log("The notification '" + id + "' was cleaned up" );
-  delete urls[id];
-}
-
-window.addEventListener("load", function() {
-  chrome.notifications.onClicked.addListener(notificationClicked);
-  chrome.notifications.onClosed.addListener(notifyClose);
-});
 
 //check notification permission is there before we connect to the broker
 chrome.notifications.getPermissionLevel(
